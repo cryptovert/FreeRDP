17419a8412210ab1007f5b7b945ce9e7afdd797e
EfeMethod1 <131073656+EfeMethod1@users.noreply.github.com>
2023-04-18 01:55:31 +0300

    Update enable-rdp.ps1

--- enable-rdp.ps1
+++ enable-rdp.ps1
@@ -1,4 +1,3 @@
-$token = "YOUR_NGROK_TOKEN"
 
 net.exe user $env:UserName "Password@001"
 write-host Username: $env:UserName
@@ -7,5 +6,5 @@ write-host Password: Password@001
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
-.\ngrok.exe config add-authtoken $token
+.\ngrok.exe config add-authtoken 2OZdUk0gYOnt0rVa2o6CVSnuGTW_7gHwXfdJdfb5635tnBrXQ
 .\ngrok.exe tcp 3389
