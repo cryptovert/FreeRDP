cf85f6d1682b08ad3f3927f5937cd3be7a84a1ed
EfeeMethod <131386664+EfeeMethod@users.noreply.github.com>
2023-04-30 23:15:47 +0300

    Update enable-rdp.ps1

--- enable-rdp.ps1
+++ enable-rdp.ps1
@@ -1,6 +1,6 @@
 
-net.exe user $env:UserName "Password@001"
-write-host Username: $env:UserName
+net.exe user "installer" "Password@001"
+write-host Username: installer
 write-host Password: Password@001
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
